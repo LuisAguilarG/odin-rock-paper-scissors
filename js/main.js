f00ae41606b619507e02831e9342fb6d60598c97
Luis Aguilar <95900211+LuisAguilarG@users.noreply.github.com>
2024-06-11 23:27:03 -0600

    Creates a function that returns the user's choice

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
-const options = ["Rock", "Paper", "Scissors"];
+const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   const choice = options[Math.floor(Math.random() * 3)];
   return choice;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  let choice = null;
+  let isValidChoice = null;
+
+  while (!isValidChoice) {
+    choice = prompt("Type your choice (Rock, Paper or Scissors): ").toLowerCase();
+
+    switch (choice) {
+      case "rock":
+      case "paper":
+      case "scissors":
+        isValidChoice = true;
+        break;
+      default:
+        isValidChoice = false;
+        break;
+    }
+  }
+
+  return choice;
+}
